c2d01e0a92e82a2d7fdc28f32c4ee3461d6a54ec
aefj11 <aefj0@163.com>
2022-06-21 21:30:49 +0800

    test.vue
    init

---
+++ test.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    <camera style="height: 500px" devicePosition="back" flash="auto" class="camera" mode="scanCode" @onScanCode="scanCode"></camera>
+  </view>
+</template>
+
+<script>
+import Taro from '@tarojs/taro'
+
+export default {
+  name: 'ws-carema',
+  methods: {
+    scanCode(data) {
+      console.log(data)
+    },
+  },
+}
+</script>
+
+
+<!-- <style lang="scss" scoped>
+@import './css/index.scss';
+</style> -->
